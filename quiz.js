a8c6e88785f213d4c15aee0cefa99492fe175619
justin leggett <justin@codingrookie.com>
2016-08-03 16:02:40 -0500

    Quiz Completed

--- quiz.js
+++ quiz.js
@@ -5,7 +5,7 @@
 
 /*
 Once the user enters in a number, and a character, the user can either then just press the
-enter key _(as long as the cursor is in one of the input fields)_, or click a button that is
+enter key as long as the cursor is in one of the input fields)_, or click a button that is
 labeled "Grow your tree" and the tree should be shown in the console. This requires you to add
 an event listener to the button, as well as an event listener for the enter/return key.
 */
@@ -14,25 +14,36 @@ an event listener to the button, as well as an event listener for the enter/retu
 the enter key, or presses the button, then display an alert stating that both fields must have a value.*/
 
 
+var tree = [];
+var line = "";
+var counter = 0;
+
 // Create a function to create the tree
+// write to an array and then output the array
 function createTree(size, character) {
-    for(var i = size; i > 0; i--) {
-        for(var j = 0; j < size; j++) {
-            console.log(" ");
+    // attempt number 2
+    size = parseInt(size);
+    var iniSize = ((parseInt(size) + 1) - parseInt(size));
+    for(var h = size; h > 0; h--) {
+        tree[counter] = "";
+        for(var i = h; i > 0; i--) {
+            tree[counter]+= "_";
         }
-        console.log(character);
+        for(var j = 0; j < iniSize; j++) {
+            tree[counter] += character;
+        }
+        console.log(tree[counter]);
+        counter++;
+        iniSize += 2;
     }
 }
 
-
-
 // Event listener for the 2 inputs
+// add event listener for user hitting enter
 document.getElementById("submit").addEventListener("click", function() {
     // add logic to display an alert if either field is empty
     var size = document.getElementById("treeSize").value;
     var character = document.getElementById("charUsed").value;
-    console.log("size = ", size, " character = ", character);
 // Call a function to create the tree
     createTree(size, character);
 });
-// output to the console
